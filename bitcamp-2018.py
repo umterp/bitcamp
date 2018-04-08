ea1c0d3d72076aa82a753dfedc18ac1d1072868b
umterp <cmuddjr@gmail.com>
2018-04-07 23:57:47 -0400

    parsed data and stored in list

--- bitcamp-2018.py
+++ bitcamp-2018.py
@@ -1,35 +1,63 @@
 import csv
 import sys
+import numpy as np
+import pandas as pd
 import urllib.request
 from urllib.request import urlopen
 import urllib.error
 from bs4 import BeautifulSoup
+import mysql
+
+company_list = []
+
+# ge_url  = "https://www.epa.gov/greenpower/green-power-partnership-national-top-100"
+# ge_page = urlopen(ge_url)
+# soup = BeautifulSoup(ge_page, 'html.parser')
+
+class Company:
+
+    def __init__(self, name):
+        self.name = name
+        self.rate = 5
+        self.values = {}
+        # self.record_values = record_values
 
 
-ge_url  = "https://www.epa.gov/greenpower/green-power-partnership-national-top-100"
+    def update_field(self, key, val):
+        self.values[key] = val
 
-ge_page = urlopen(ge_url)
-soup = BeautifulSoup(ge_page, 'html.parser')
-
-
-water_file = open('/Users/Documents/bitcamp_data/water_data.csv', "rb")
-emissions_file = open('/Users/Documents/bitcamp_data/emissions_data.csv', "rb")
-forest_file = open('/Users/Documents/bitcamp_data/forest_data.csv', "rb")
-
-class ParentCompany:
-
-    def __init__(self, id, rate, record_values):
-        self.id = id
-        self.rate = rate
-        if not hasattr(self, record_values):
-            record_values = {}
-        self.record_values = {**self.record_values, **record_values}
 
     def show_rate(self):
         print(self.rate)
 
 
-class Company(ParentCompany):
+with open('/Users/dirtydan/Github/personal-projects/bitcamp-2018/water_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row[6] != 'No':
+            new_company = Company(row[0])
+            new_company.update_field('water policy', 'Yes')
+            company_list.append(new_company)
+        else:
+            new_company = Company(row[0])
+            new_company.update_field('water policy', 'No')
+            company_list.append(Company(row[0]))
 
-    def __init__(self, id, rate, parent_company):
-        ParentCompany.__init__(self, id, rate, {"Parent Company": parent_company})
+emissions_file = open('/Users/dirtydan/Github/personal-projects/bitcamp-2018/emissions_data.csv', 'r')
+with open('/Users/dirtydan/Github/personal-projects/bitcamp-2018/emissions_data.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        for i, j in enumerate(company_list):
+            if j.__dict__['name'] == row[0] and row[7]:
+                company_list[i].update_field('emissions', row[7])
+
+
+with open('/Users/dirtydan/Github/personal-projects/bitcamp-2018/forest_data.csv') as f:
+    reader = csv.reader(f)
+    for i, j in enumerate(company_list):
+        if j.__dict__['name'] == row[0] and row[9] != 'No risk':
+            company_list[i].update_field('forestry', 'risks present')
+
+
+company_list.pop(0)
+print(list(map(lambda x: x.__dict__, company_list)))
